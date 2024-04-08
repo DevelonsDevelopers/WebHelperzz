00618af505c6346b638636a136961170cde42e7e
Tayyab Mazhar <tayyabrana213@gmail.com>
2024-04-08 06:49:21 +0500

    URL changed

--- src/api/services/axiosInstance.js
+++ src/api/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://develons.site/api',
+    baseURL: 'https://api.helperzz.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
