f8179e76f2b4f56ae756964e4b38f8b4b46a492a
Tayyab Mazhar <tayyabrana213@gmail.com>
2024-04-08 14:37:46 +0500

    Loading Changed

--- src/components/loading.js
+++ src/components/loading.js
@@ -6,9 +6,9 @@ export default function Loading() {
     <>
       <div class="flex space-x-2 justify-center items-center bg-white h-screen">
         <span class="sr-only">Loading...</span>
-        <div class="h-6 w-6 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-        <div class="h-6 w-6 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-        <div class="h-6 w-6 bg-black rounded-full animate-bounce"></div>
+        <div class="h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div class="h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div class="h-4 w-4 bg-black rounded-full animate-bounce"></div>
       </div>
     </>
   );
