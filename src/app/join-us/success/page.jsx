e09d89147392f3e6bfed02b5f41e62d45c20cb10
Tayyab Mazhar <tayyabrana213@gmail.com>
2024-06-04 22:33:57 +0500

    Join Us Success

--- src/app/join-us/success/page.jsx
+++ src/app/join-us/success/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
  import seoService from "@/api/services/seoService";
  import SuccessComponent from './SuccessComponent'
- 
+
 export async function generateMetadata() {
 
-    const res = await seoService.fetchSEObyRoute("join-us/success")
+    const res = await seoService.fetchSEObyRoute("join-us-success")
 
     return {
         title: res?.seo?.title,
